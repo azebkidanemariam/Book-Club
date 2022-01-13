a6d0c63b695836f6aa2dba0e4479c866b6b19c38
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2022-01-13 22:24:39 +0100

    small chang on file structure

--- src/pages/MeetupDetail/event-unit.test.tsx
+++ src/pages/MeetupDetail/event-unit.test.tsx
@@ -4,78 +4,78 @@ import MeetupDetail from './MeetupDetail'
 
 
 
-describe('Meetup detail unit tests', () => {
-  it('renders the meetup detail component (smoke test)', () => {
+describe('Event detail unit tests', () => {
+  it('renders the event detail component (smoke test)', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
   })
  
-  it('displays the start date', () => {
+  it('render the opening date', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const startDate = screen.getByText('Sat Dec 17 2022 13:00', { exact: false })
     expect(startDate).toBeInTheDocument()
   })
-  it('displays the end date', () => {
+  it('render the final date', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const endDate = screen.getByText('Sat Dec 17 2022 15:00', { exact: false })
     expect(endDate).toBeInTheDocument()
   })
-  it('displays the location', () => {
+  it('render location', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const location = screen.getByText('10 Main Street, London', { exact: false })
     expect(location).toBeInTheDocument()
   })
  
-  it('displays the description', () => {
+  it('render description', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const description = screen.getByText('Description', { exact: false })
     expect(description).toBeInTheDocument()
   })
-  it('displays the price', () => {
+  it('render price', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const price = screen.getByText('Price', { exact: false })
     expect(price).toBeInTheDocument()
   })
-  it('displays the attendees', () => {
+  it('render attendees', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const attendeesList = screen.getAllByTestId('userCard')
     expect(attendeesList).toHaveLength(2)
   })
-  it('doesnt render a text area input for comments if user is logged out', () => {
+  it('do not dsplay a text area input for comments for unauthorized user', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const commentInput = screen.queryByLabelText('Add a comment')
     expect(commentInput).not.toBeInTheDocument()
   })
-  it('doesnt render an add comment button if user is logged out', () => {
+  it('do not display add comment button for unauthorized user', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const addCommentButton = screen.queryByRole('button', { name: 'Add' })
     expect(addCommentButton).not.toBeInTheDocument()
   })
-  it('renders a list of empty comments initially for meetup with id 1', () => {
+  it('renders no comments initially for meetup with id 1', () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const commentsList = screen.queryAllByTestId('commentListItem')
     expect(commentsList).toHaveLength(0)
   })
-  it('renders a list of 2 comments initially for meetup with id 3', () => {
+  it('renders  2 comments initially for meetup with id 3', () => {
     renderWithPath('/meetups/3', <MeetupDetail />, '/meetups/:id')
     const commentsList = screen.queryAllByTestId('commentListItem')
     expect(commentsList).toHaveLength(2)
   })
-  it("does not render a rating for future meetups", () => {
+  it("does not render a rating for coming events", () => {
     renderWithPath('/meetups/1', <MeetupDetail />, '/meetups/:id')
     const ratingContainer = screen.queryByTestId("rating")
     expect(ratingContainer).not.toBeInTheDocument()
   })
-  it("renders a rating for past meetups", () => {
+  it("renders a rating for previous events", () => {
     renderWithPath('/meetups/3', <MeetupDetail />, '/meetups/:id')
     const ratingContainer = screen.queryByTestId("rating")
     expect(ratingContainer).toBeInTheDocument()
   })
-  it("renders a rating of 4.5 for  meetup with id 3", () => {
+  it("renders a rating of 4.5 for  event with id 3", () => {
     renderWithPath('/meetups/3', <MeetupDetail />, '/meetups/:id')
     const rating = screen.getByText("4.5 / 5")
     expect(rating).toBeInTheDocument()
   })
-  it("renders a no ratings yet message for  meetup with id 5", () => {
+  it("renders a no ratings yet message for  event with id 5", () => {
     renderWithPath('/meetups/5', <MeetupDetail />, '/meetups/:id')
     const ratingMessage = screen.getByText("No ratings yet")
     expect(ratingMessage).toBeInTheDocument()
